bd71026f24b0a3f2f428f8408547d20d8806e734
José Cameijo Cerdeira <cerdeira@co.sapo.pt>
2020-03-26 11:06:03 +0000

    added text::template

--- perl/lib/site_perl/5.10.1/Dancer/Continuation/Route/FileSent.pm
+++
@@ -1,9 +0,0 @@
-package Dancer::Continuation::Route::FileSent;
-
-use strict;
-use warnings;
-use Carp;
-
-use base qw(Dancer::Continuation::Route);
-
-1;
--- perl/lib/site_perl/5.10.1/Dancer/Continuation/Route/Templated.pm
+++
@@ -1,9 +0,0 @@
-package Dancer::Continuation::Route::Templated;
-
-use strict;
-use warnings;
-use Carp;
-
-use base qw(Dancer::Continuation::Route);
-
-1;
