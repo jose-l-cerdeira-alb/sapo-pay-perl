59ff7c9edcc3b6c4d3b6f9e9b1cc4803eb7a29be
José Cameijo Cerdeira <cerdeira@co.sapo.pt>
2021-02-25 10:27:04 +0000

    removed unused files

--- tools/card_mobili/checkout_program_subscription.sh
+++
@@ -1,7 +0,0 @@
-curl \
-    -X POST \
-    -d '{"operation_id":"05b58399-fce5-4312-965a-05bab6149bdb","decision":"ACCEPT"}' \
-    http://payws.local/api/v2/loyalty/subscribers/cards/templates/20 \
-    -H "Authorization: WalletPT 7a0eb41208209639eda9bf765b6cb04d59fb9e34" \
-    -H "Content-Type: application/json" | python -mjson.tool
-
